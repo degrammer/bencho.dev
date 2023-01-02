ba949beaeb7d341f7c5942586fa47edc8ec021b7
degrammer <ruben@fusebit.io>
2023-01-02 18:17:17 -0500

    Add metadata

--- components/layout.js
+++ components/layout.js
@@ -2,12 +2,15 @@ import Head from 'next/head';
 import Footer from './footer';
 import Header from './header';
 
-export default function Layout({ children, title, description }) {
+export default function Layout({ children, title, description, ogImage }) {
+  const defaultImage = `${process.env.BASE_PATH}/benchologo.svg`;
   return (
     <>
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
+        <meta property="og:title" content={description} key="title" />
+        <meta property="og:image" content={ogImage || defaultImage} />
       </Head>
       <div className="container">
         <Header />
