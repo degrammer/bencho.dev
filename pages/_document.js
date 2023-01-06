b721c122b320e9588f4b44b1a6e3e12ad53e6bf8
degrammer <ruben@fusebit.io>
2023-01-06 13:23:13 -0500

    Add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,16 @@ export default function Document() {
   return (
     <Html lang="en-US">
       <Head>
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-3LYYY7B70K" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-3LYYY7B70K');
+        `}
+        </Script>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap" />
       </Head>
