84204eb76b2224a81503b6bfd235edd68adb815d
Ruben Restrepo <ruben@stateful.com>
2023-02-10 16:47:05 -0500

    Fix vscode redirect (try to)

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: '/runme-getting-started',
-        destination: 'vscode://stateful.runme?command=setup&repository=git@github.com:degrammer/runme-getting-started.git&fileToOpen=README.md',
+        destination: 'vscode:///stateful.runme?command=setup&repository=git@github.com:degrammer/runme-getting-started.git&fileToOpen=README.md',
         permanent: true,
       },
       {
