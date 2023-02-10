c3a01328ec569fb8eeba93d8585c6070658ccce6
Ruben Restrepo <ruben@stateful.com>
2023-02-10 17:05:14 -0500

    Fix vscode redirect (try to)

--- pages/api/runme.js
+++
@@ -1,3 +0,0 @@
-export default function handler(req, res) {
-    res.redirect(308, 'vscode://stateful.runme?command=setup&repository=git@github.com:degrammer/runme-getting-started.git&fileToOpen=README.md')
-  }
---
+++ pages/api/runme/[repository][open].js
@@ -0,0 +1,5 @@
+export default function handler(req, res) {
+    const { repository, open } = req.query
+    res.end(`${repository} ${open}`)
+    //res.redirect(308, 'vscode://stateful.runme?command=setup&repository=git@github.com:degrammer/runme-getting-started.git&fileToOpen=README.md')
+  }
